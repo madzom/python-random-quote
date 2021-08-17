8d89429880653b5e7d6e0f29ef32a7fd8b9056af
M M <marko.madzarac@zav-sava.si>
2021-08-17 10:12:47 +0200

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,10 +1,24 @@
+import random
+
+
 def treain():
     print("Keep it logically awesome.")
 
-    #f = open("quotes.txt")
-    #quotes = f.readlines()
-    # f.close()
+    f = open("quotes.txt")
+    quotes = f.readlines()
+    f.close()
 
-    # print(quotes)
+    print(quotes)
+    print("\n")
+    print(quotes[0])
+    print("\n")
+    print(len(quotes))
+    print(quotes[13])
+
+    last = len(quotes) - 1
+    rnd = random.randint(0, last)
+    print(quotes[rnd])
+
+
 if __name__ == "__main__":
     treain()
